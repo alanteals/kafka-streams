e1a9a577d0285999d1f261b9c26a619c899657de
allanaste <allanaste@icloud.com>
2021-01-13 16:58:56 +0100

    Write a first Streams application

---
+++ src/main/kotlin/com.allanaste.kafka/PipeApplication.kt
@@ -0,0 +1,12 @@
+package com.allanaste.kafka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class `PipeApplication` {
+    fun main(args: Array<String>) {
+
+        runApplication<`PipeApplication.kt`>(*args)
+    }
+}
